7009268919bb2ffca6d1d81938aa6958c8c6bcec
Alan Robinson <Cerberin@users.noreply.github.com>
2015-05-11 20:50:12 -0700

    Added another cell that works well...

--- tictactoe.js
+++ tictactoe.js
@@ -1,4 +1,9 @@
-function xo() {
+function xo1() {
 	var first = document.getElementById("id1");
 	first.innerHTML = "X";	
 }
+
+function xo2() {
+	var second = document.getElementById("id2");
+	second.innerHTML = "X";
+}	
