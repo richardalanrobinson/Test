b2f316677f48fc94fc0c38ad86e3d43f45d2b3a7
Alan Robinson <Cerberin@users.noreply.github.com>
2015-05-11 20:59:51 -0700

    DRY function improvement

--- tictactoe.js
+++ tictactoe.js
@@ -1,4 +1,4 @@
-function xo1() {
+/*function xo1() {
 	var first = document.getElementById("id1");
 	first.innerHTML = "X";	
 }
@@ -6,4 +6,9 @@ function xo1() {
 function xo2() {
 	var second = document.getElementById("id2");
 	second.innerHTML = "X";
+}*/	
+
+function xo(id) {
+	var cell = document.getElementById(id);
+	cell.innerHTML = "X";
 }	
