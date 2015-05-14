47d21da86edd6d4c43c76e2146375f94b6aea03e
Alan Robinson <Cerberin@users.noreply.github.com>
2015-05-13 20:32:20 -0700

    Made the strXo Boolean.

--- tictactoe.js
+++ tictactoe.js
@@ -1,14 +1,25 @@
-/*function xo1() {
-	var first = document.getElementById("id1");
-	first.innerHTML = "X";	
+function xo(id) {
+	var elementCell = document.getElementById(id);
+	if (strXo == "X") {
+		elementCell.innerHTML = "X";
+		strXo = "O";
+	} else {
+		elementCell.innerHTML = "O";
+		strXo = "X";
+	}
+
 }
 
-function xo2() {
-	var second = document.getElementById("id2");
-	second.innerHTML = "X";
-}*/	
+var strXo = "X";
 
-function xo(id) {
-	var cell = document.getElementById(id);
-	cell.innerHTML = "X";
-}	
+
+
+
+
+
+
+
+
+
+
+	
